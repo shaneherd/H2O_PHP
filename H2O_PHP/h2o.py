2b3defee1f1e20e81399d3023ca3be25559e0072
Herd <ssherds@gmail.com>
2014-07-13 13:12:54 -0600

    added freakduinos pseudo code

--- H2O_PHP/h2o.py
+++ H2O_PHP/h2o.py
@@ -46,7 +46,70 @@ for freakduino in freakduinosList:
     i = i + 1
     
 '''
-get a list of all of the freakduinos
-get a list of all of the nodes for the specific freakduinos
-get a list of all of the valves for the specific nodes
+This script is started by the h2o.sh script when the initialize pin has been set high
+Get all of the active valves
+Get amounts to sned to each of the valves
+Combine address with the amount
+Send data to freakduinos without starting
+    i = 0
+    j = 0
+    for freakduino in freakduinosList:
+        for node in allNodesList[i]:
+            for valve in allValvesList[j]:
+                noResponse = true
+                while noResponse:
+                    send combined address and amount to freakduino
+                    look for response from freakduino
+                    if response found
+                        noResponse = false
+                j = j + 1
+            i = i + 1
+    At this point, all data has been sent
+    Set initialize pin in database low
+    Set ready pin in database high so that the user can press the start button in the app
+    
+start = 0
+stop = 0
+while start == 0:
+    start = start pin valve from database
+
+Set start pin in databse low    
+numToRunAtSameTime = 3
+i = 0
+numFreakduinos = freakduinosList.len()
+numDone = 0
+
+start the first # of freakduinos
+while i < numToRunAtSameTime:
+    send start to freakduinosList[i]
+    i = i + 1
+    
+while not all are done
+while numDone < numFreakduinos and stop == 0:
+    while not all are started
+    while i < numFreakduinos and stop == 0:
+        almostDone = false
+        stop = stop pin value from database
+        if stop == 1:
+            send stop signal to all active freakduinos
+        else:
+            listen for almost done signal from active freakduinos
+            if almostDone:
+                send confirmation of almostDone receieved to sending freakduino so that it will stop sending
+                send start to freakduinosList[i]
+                numDone = numDone + 1
+                i = i + 1
+    
+    finish remaining freakduinos
+    stop = stop pin value from database
+    if stop == 1:
+        send stop signal to all active freakduinos
+    else:
+        listen for almost done signal from active freakduinos
+        if almostDone:
+            send confirmation of almostDone receieved to sending freakduino so that it will stop sending
+            numDone = numDone + 1
+
+All valves are done or have been manually stopped
+Set stop pin in database low
 '''
